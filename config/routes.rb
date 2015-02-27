d09ae1fdcb850bf7142d53c5033b8a07f1a1b95a
DanyTlaw <daniel.herzogfhnw@gmail.com>
2015-02-27 15:42:46 +0100

    login in der navi routing prob bei voting

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
   resources :champions do
-    resources :counters
+    resources :counters do
+    	member do
+    		put "like" => "counters#upvote"
+    		put "unlike" => "counters#downvote"
+    	end
+    end
   end
 
   root 'champions#index'
 
   devise_for :users
-                                                                                                                                
+                              
+
 end
