8e3dd5004248d17ff63c561d8b192b1c24a13acb
Bloem17 <damiano.negro@gmx.net>
2015-03-05 20:39:00 +0100

    Kalista hinzugefügt, Einheitliche Positionsbezeichnungen und fix fuer Bug im Match Up Javascript

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227193651) do
+ActiveRecord::Schema.define(version: 20150303133450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 20150227193651) do
     t.string   "champ_name"
     t.string   "champ_gegner"
     t.integer  "champion_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "info"
+    t.string   "image"
   end
 
   create_table "matchups", force: :cascade do |t|
