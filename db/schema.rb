5e1467b7300309a4d029a0d494b7fc262e7628d3
notyves <yps.notter@gmail.com>
2015-03-17 10:59:14 +0100

    DB problems solved, seed one user

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150312141437) do
-=======
-ActiveRecord::Schema.define(version: 20150303083000) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
